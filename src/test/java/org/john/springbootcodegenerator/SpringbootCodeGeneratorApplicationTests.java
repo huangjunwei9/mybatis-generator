b2822101f824fc6fc6ff825c717108cbed0304ae
John Deng <591019221@qq.com>
2019-06-19 11:40:03 +0800

    添加测试用例

--- src/test/java/org/john/springbootcodegenerator/SpringbootCodeGeneratorApplicationTests.java
+++ src/test/java/org/john/springbootcodegenerator/SpringbootCodeGeneratorApplicationTests.java
@@ -1,6 +1,7 @@
 package org.john.springbootcodegenerator;
 
 import org.john.springbootcodegenerator.service.impl.TemplateDefaultGeneratorServiceImpl;
+import org.john.springbootcodegenerator.service.impl.TemplateNeedExtendGeneratorServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,25 +18,41 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringbootCodeGeneratorApplicationTests {
 	
 	@Autowired
-	public TemplateDefaultGeneratorServiceImpl codeGenerator;
-//	@Autowired
-//	public TemplateNeedExtendGeneratorServiceImpl codeGenerator
+	public TemplateDefaultGeneratorServiceImpl templateDefaultGeneratorServiceImpl;
+	@Autowired
+	public TemplateNeedExtendGeneratorServiceImpl templateNeedExtendGeneratorServiceImpl;
 	
 	/**
-	 * 
+	 * 使用默认（不继承类）生成代码
 	 * @author JohnDeng
 	 * @datatime 2019年1月21日下午4:14:55
 	 */
 	@Test
-	public void codeGeneratorTest(){
-//		String tableName="tb_account_user",classDescription="系统用户管理";
+	public void defaultGeneratorTest(){
 		String tableName="tb_sys_user",classDescription="系统用户管理";
-		codeGenerator.createEntityTemplate(tableName, classDescription);
-		codeGenerator.createDaoTemplate(tableName, classDescription);
-		codeGenerator.createServiceTemplate(tableName, classDescription);
-		codeGenerator.createServiceImplTemplate(tableName, classDescription);
-		codeGenerator.createControllerTemplate(tableName, classDescription);
-		codeGenerator.createMapperTeplate(tableName, classDescription);
+		templateDefaultGeneratorServiceImpl.createEntityTemplate(tableName, classDescription);
+		templateDefaultGeneratorServiceImpl.createDaoTemplate(tableName, classDescription);
+		templateDefaultGeneratorServiceImpl.createServiceTemplate(tableName, classDescription);
+		templateDefaultGeneratorServiceImpl.createServiceImplTemplate(tableName, classDescription);
+		templateDefaultGeneratorServiceImpl.createControllerTemplate(tableName, classDescription);
+		templateDefaultGeneratorServiceImpl.createMapperTeplate(tableName, classDescription);
+		
+	}
+	
+	/**
+	 * 使用需要继承类生成代码
+	 * @author JohnDeng
+	 * @dateTime 2019年6月19日上午11:26:06
+	 */
+	//@Test
+	public void needExtendGeneratorTest(){
+		String tableName="tb_sys_user",classDescription="系统用户管理";
+		templateNeedExtendGeneratorServiceImpl.createEntityTemplate(tableName, classDescription);
+		templateNeedExtendGeneratorServiceImpl.createDaoTemplate(tableName, classDescription);
+		templateNeedExtendGeneratorServiceImpl.createServiceTemplate(tableName, classDescription);
+		templateNeedExtendGeneratorServiceImpl.createServiceImplTemplate(tableName, classDescription);
+		templateNeedExtendGeneratorServiceImpl.createControllerTemplate(tableName, classDescription);
+		templateNeedExtendGeneratorServiceImpl.createMapperTeplate(tableName, classDescription);
 		
 	}
 	
